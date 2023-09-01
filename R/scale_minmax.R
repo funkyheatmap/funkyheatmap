1b2a5a545fd95d9f731a6facf4999ffea1002412
Robrecht Cannoodt <rcannood@gmail.com>
2023-09-01 21:06:11 +0200

    export scale_minmax

--- R/scale_minmax.R
+++ R/scale_minmax.R
@@ -1,3 +1,11 @@
+#' Scale a vector to the range [0, 1]
+#' 
+#' @param x A numeric vector
+#' @return A numeric vector
+#' @export
+#' 
+#' @examples
+#' scale_minmax(c(1, 2, 3))
 scale_minmax <- function(x) {
   minx <- min(x, na.rm = TRUE)
   maxx <- max(x, na.rm = TRUE)
