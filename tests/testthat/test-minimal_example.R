7d3c3ee32dd070e2761958dcefe9a823eb10fb92
Louise Deconinck <louise.deconinck@gmail.com>
2024-11-04 10:50:23 +0100

    Change directories for test

--- tests/testthat/test-minimal_example.R
+++ tests/testthat/test-minimal_example.R
@@ -3,7 +3,7 @@ require(jsonlite)
 require(readr)
 
 # dir <- "test_data/minimal_"
-dir <- "tests/testthat/test_data/minimal_"
+dir <- "test_data/minimal_"
 
 data <- readr::read_tsv(paste0(dir, "data.tsv")) %>%
   mutate_at(
@@ -37,7 +37,7 @@ legends <- list(
     enabled = TRUE,
     geom = "image",
     size = 3.88,
-    labels = c("tests/testthat/test_data/one.png", "tests/testthat/test_data/two.png", "tests/testthat/test_data/three.png"),
+    labels = c("test_data/one.png", "test_data/two.png", "test_data/three.png"),
     values = c("One", "Two", "Three")
   )
 )
